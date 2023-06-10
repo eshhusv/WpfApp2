9606b471919845fddbdb3ab9d25c9cf2d663c447
ak752 <ak752@DESKTOP-VOSGIAN>
2023-06-10 18:47:04 +0200

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using WpfApp2;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<int> mas = new List<int>();
+        private List<double> masReal = new List<double>();
+        private Queue<Triangle> queueReal = new Queue<Triangle>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            // lbLab8.ItemsSource = intList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Triangle triangle = new Triangle(double.Parse(triangleleg1.Text), double.Parse(triangleleg2.Text));
+            queueReal.Enqueue(triangle);
+            TreeViewItem item = new TreeViewItem();
+            item.Tag = "Запись " + queueReal.Count;
+            item.Header = "Запись " + queueReal.Count;
+            item.Items.Add(triangle.leg1);
+            item.Items.Add(triangle.leg2);
+            treeList.Items.Add(item);
+            triangleleg1.Clear();
+            triangleleg2.Clear();
+        }
+
+    }
+}
---
+++ WpfApp2/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class Triangle
+    {
+        public double leg1 { get; set; }
+        public double leg2 { get; set; }
+        public Triangle(double Leg1, double Leg2)
+        {
+            Leg1 = leg1;
+            Leg2 = leg2;
+        }
+    }
+}
